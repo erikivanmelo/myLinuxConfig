4f36d39624a3a1cb79f453806999c43826c3ba02
Erik Ivan Melo <erikivanmelog@gmail.com>
2025-09-07 10:06:36 -0300

    simplified and updated nvim maps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,3 +1,192 @@
--- Aggregator: load basic and plugin keymaps
-pcall(function() require('maps.basic') end)
-pcall(function() require('maps.plugins') end)
+-- maps/basic.lua - basic (non-plugin) keymaps
+local map = vim.keymap.set
+local silent = { silent = true }
+
+-- Leader
+vim.g.mapleader = ' '
+
+-- Terminal key sequences for Home/End
+map({ 'n', 'v', 's', 'o' }, '<ESC>[8~', '<End>', silent)
+map({ 'n', 'v', 's', 'o' }, '<ESC>[7~', '<Home>', silent)
+map('i', '<ESC>[8~', '<End>', silent)
+map('i', '<ESC>[7~', '<Home>', silent)
+
+-- Vertical split
+map('n', '<F4>', ':vsplit<CR>', silent)
+map('i', '<F4>', '<Esc>:vsplit<CR>i', silent)
+
+-- Find
+map('n', '<leader>ff', ':lua Snacks.picker.files()<cr>', silent)
+map('n', '<leader>fg', ':lua Snacks.picker.lines()<cr>', silent)
+map('n', '<leader>fc', ':lua Snacks.picker.grep()<cr>', silent)
+map('n', '<Leader>fb', ':lua Snacks.picker.buffers()<cr>', silent)
+map('n', '<Leader>fe', ':lua Snacks.explorer()<cr>', silent)
+map('n', '<Leader>fr', ':RnvimrToggle<cr>', silent)
+
+-- Text navigation
+map('n', '<S-Up>', ':m-2<CR>', silent)
+map('n', '<S-Down>', ':m+<CR>', silent)
+map('i', '<S-Up>', '<Esc>:m-2<CR>i<Right>', silent)
+map('i', '<S-Down>', '<Esc>:m+<CR>i<Right>', silent)
+
+map('v', '<PageUp>', '10<Up>', silent)
+map('v', '<PageDown>', '10<Down>', silent)
+map('n', '<PageUp>', '10<Up>', silent)
+map('n', '<PageDown>', '10<Down>', silent)
+map('i', '<PageUp>', '<Esc>10<Up>i', silent)
+map('i', '<PageDown>', '<Esc>10<Down>i', silent)
+
+map('n', '<S-Left>', '<C-O>', silent)
+map('n', '<S-Right>', '<C-I>', silent)
+
+map('n', '<C-Home>', 'gg', silent)
+map('n', '<C-End>', 'G', silent)
+
+-- Toggle highlights and numbers
+map('n', '<F3>', ':set hlsearch!<CR>', silent)
+map('i', '<F3>', '<Esc>:set hlsearch!<CR>i<Right>', silent)
+map('n', '<leader>nr', ':set relativenumber!<CR>', silent)
+
+-- Replace text helpers
+map('i', '<C-M-f>', '<Esc>:%s///gc<Left><Left><Left><Left>')
+map('n', '<C-M-f>', ':%s///gc<Left><Left><Left><Left>')
+map('v', '<C-M-f>', 'y:%s/<C-R>"//gc<Left><Left><Left>')
+
+-- Delete helpers
+map('n', '<C-Del>', 'dw', silent)
+map('n', '<C-Backspace>', 'bdw', silent)
+map('v', '<Del>', '"_d', silent)
+map('n', '<Del>', '"_<Del>', silent)
+map('n', '<Backspace>', 'X', silent)
+
+-- :W user command and save mappings
+vim.api.nvim_create_user_command('W', function()
+  local path = vim.fn.expand('%:p')
+  if vim.fn.filereadable(path) == 1 then
+    vim.cmd('write')
+  else
+    local nuevo = vim.fn.input('Filename: ')
+    if nuevo ~= '' then
+      vim.cmd('write ' .. nuevo)
+    end
+  end
+end, {})
+map('i', '<C-s>', '<Esc>:W<CR>', silent)
+map('n', '<C-s>', ':W<CR>', silent)
+
+-- :Tabclose user command and helpers
+vim.api.nvim_create_user_command('Tabclose', function()
+  local bufnr = vim.fn.bufnr('%')
+  local wins = vim.fn.win_findbuf(bufnr)
+  if #wins > 1 then
+    vim.cmd('q')
+    return
+  end
+  local last = vim.fn.bufnr('$')
+  local listed = 0
+  for b = 1, last do
+    if vim.fn.buflisted(b) == 1 then
+      listed = listed + 1
+    end
+  end
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  if listed == 1 and (#lines == 1 and lines[1] == '') then
+    vim.cmd('q!')
+  else
+    vim.cmd('bd')
+  end
+end, {})
+
+-- Unmap defaults that conflict and remap window close
+pcall(vim.keymap.del, 'n', '<C-W><C-D>')
+pcall(vim.keymap.del, 'n', '<C-W>d')
+map('i', '<C-w>', '<Esc>:Tabclose<CR>i', silent)
+map('n', '<C-w>', ':Tabclose<CR>', silent)
+
+-- Tabs
+map('i', '<C-t>', '<Esc>:tabnew<CR>i', silent)
+map('n', '<C-t>', ':tabnew<CR>', silent)
+map('n', '<Leader><C-t>', ':new tab<cr>', silent)
+map('n', '<Leader><C-w>', ':bd<cr>', silent)
+
+-- Tab navigation Alt+1..9
+for i = 1, 9 do
+  local key = string.format('<M-%d>', i)
+  map('n', key, string.format(':tabn %d<CR>', i), silent)
+  map('i', key, string.format('<Esc>:tabn %d<CR>i', i), silent)
+  map('v', key, string.format('<Esc>:tabn %d<CR>', i), silent)
+end
+
+-- Misc
+map('i', '<C-D>', '<C-W>', silent)
+map('n', '<C-a>', 'ggVG', silent)
+map('n', '<F12>', 'i', silent)
+map('v', '<F12>', '<Esc>', silent)
+map('i', '<F12>', '<Right><Esc>', silent)
+map('n', '<F5>', ':e<CR>', silent)
+map('i', '<F5>', 'i:e<CR><right>i', silent)
+map('n', 'qq', ':q!<CR>', silent)
+
+-- Remap <C-u>
+map('n', '<C-u>', '<C-r>', silent)
+
+-- Git
+map('n', '<F1>', ':GitBlame<CR>', silent)
+map('i', '<F1>', '<Right><Esc>:GitBlame<CR>i', silent)
+map('n', '<leader>gc', ':!git commit -m ""<Left>')
+map('n', '<leader>gp', ':!git pull<CR>', silent)
+map('n', '<leader>gP', ':!git push<CR>', silent)
+map('n', '<leader>gC', ':lua Snacks.picker.git_branches()<cr>', silent)
+map('n', '<F6>', ':DiffviewOpen<cr>', silent)
+map('i', '<F6>', '<Esc>:DiffviewOpen<cr>', silent)
+map('n', '<leader><F6>', ':DiffviewClose<cr>', silent)
+
+-- LSP
+ --Navegación de código
+--map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', silent)
+--map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', silent)
+--map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', silent)
+--map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', silent)
+--map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', silent)
+--map('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', silent)
+--map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', silent)
+--map('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', silent)
+
+--Diagnosticos
+--map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', silent)
+--map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', silent)
+--map('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', silent)
+--map('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', silent)
+
+-- Completado inteligente
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true })
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', { expr = true })
+
+-- tmux navigator
+    -- left
+    map('n', '<C-g>', ':TmuxNavigateLeft<cr>', silent)
+    map('i', '<C-g>', '<Esc>:TmuxNavigateLeft<cr>i', silent)
+
+    -- up
+    map('n', '<C-h>', ':TmuxNavigateUp<cr>', silent)
+    map('i', '<C-h>', '<Esc>:TmuxNavigateUp<cr>', silent)
+
+    -- right
+    map('n', '<C-l>', ':TmuxNavigateRight<cr>', silent)
+    map('i', '<C-l>', '<Esc>:TmuxNavigateRight<cr>i', silent)
+
+    -- down
+    map('n', '<C-c>', ':TmuxNavigateDown<cr>', silent)
+    map('i', '<C-c>', '<Esc>:TmuxNavigateDown<cr>', silent)
+
+-- Checklist plugin
+map('n', '<leader>ct', ':ChecklistToggleCheckbox<cr>', silent)
+map('n', '<leader>ce', ':ChecklistEnableCheckbox<cr>', silent)
+map('n', '<leader>cd', ':ChecklistDisableCheckbox<cr>', silent)
+map('v', '<leader>ct', ':ChecklistToggleCheckbox<cr>', silent)
+map('v', '<leader>ce', ':ChecklistEnableCheckbox<cr>', silent)
+map('v', '<leader>cd', ':ChecklistDisableCheckbox<cr>', silent)
+
+-- NERDCommenter
+map('n', '<leader>cc', '<plug>NERDCommenterToggle', silent)
+map('v', '<leader>cc', '<plug>NERDCommenterToggle', silent)
